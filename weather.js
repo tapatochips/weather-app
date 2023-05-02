fe50fdcd007afcb4df1ccefd74b5f916f2fbb0b0
Daniel Richmond <danielrichmond@Daniels-MacBook-Pro.local>
2023-05-02 11:16:21 -0500

    added comments

--- weather.js
+++ weather.js
@@ -1,21 +1,25 @@
+//let weather connects to the api
 let weather = {
-    "apiKey": "7879540023988757c983f86d8a0e5540",
-    fetchWeather: function(city) {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-              city +
-              "&units=imperial&appid=" +
-              this.apiKey
-          )
-            .then((response) => {
-              if (!response.ok) {
-                alert("No weather found.");
-                throw new Error("No weather found.");
-              }
-              return response.json();
-            })
-            .then((data) => this.displayWeather(data));
-        },
+  "apiKey": "7879540023988757c983f86d8a0e5540",
+  fetchWeather: async function(city) {
+      try {
+          const response = await fetch(
+              "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=imperial&appid=" +
+                this.apiKey
+          );
+          if (!response.ok) {
+              alert("No weather found.");
+              throw new Error("No weather found.");
+          }
+          const data = await response.json();
+          this.displayWeather(data);
+      } catch (error) {
+          console.error(error);
+      }
+  },
+  //display weather pulls specific data from api and displays it to the user based on their search
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
